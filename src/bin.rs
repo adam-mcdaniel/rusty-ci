f7c08f9f3e701089ae05e1933a806256a1f64161
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-13 22:45:59 -0400

    Added setup subcommand

--- src/bin.rs
+++ src/bin.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rusty_ci;
 
-use rusty_ci::File;
+use rusty_ci::{File, input};
 use rusty_ci::{BuildSystem, DefaultBuildSystem, Bash, Makefile, MasterConfig, Worker};
 use rusty_yaml::Yaml;
 use std::process::exit;
@@ -28,6 +28,16 @@ fn main() {
                         (author: "Adam McDaniel <adam.mcdaniel17@gmail.com>")
                         (@arg YAML: +required "The path to the YAML file")
                     )
+                    (@subcommand setup => 
+                        (about: "Output a template YAML file for you to change to customize")
+                        (version: "0.0.1")
+                        (author: "Adam McDaniel <adam.mcdaniel17@gmail.com>")
+                    )
+                    // (@subcommand tutorial => 
+                    //     (about: "")
+                    //     (version: "0.0.1")
+                    //     (author: "Adam McDaniel <adam.mcdaniel17@gmail.com>")
+                    // )
         ).setting(AppSettings::ArgRequiredElseHelp)
             .get_matches();
     
@@ -59,15 +69,151 @@ fn main() {
             let yaml = Yaml::from(content);
             build(buildsystem, yaml)
         },
+        Some("setup") => {
+            match setup() {
+                Ok(_) => {},
+                Err(e) => {
+                    error!("There was a problem writing the template yaml file: {}", e);
+                    exit(1);
+                }
+            };
+        },
         _ => {}
     }
 }
 
 
 
+/// This function writes a template YAML file for the user to edit as needed.
+fn setup() -> Result<(), String> {
+    let filename = input("Where do you want the output file to be? ");
+    info!("Writing template yaml file to {}...", filename);
+    File::write(filename, r#"
+# This section holds data specific to the master of the workers
+master:
+  # The title subsection of the master holds the title of your web gui
+  title:
+    - "Rusty-CI"
+  title-url:
+    - "https://github.com/adam-mcdaniel/rusty-ci"
+
+  # This is the ip of the web-gui
+  # The port is 8010
+  webserver-ip:
+    - localhost
+
+  # The address of your repository
+  repo:
+    - "https://github.com/adam-mcdaniel/rusty-ci"
+
+  # The number of seconds to wait before checking for updates on your repository
+  poll-interval:
+    - 10
+
+# This section holds data specific to the handler that will look for 
+# pull requests / merge requests on your repository
+merge-request-handler:
+  # This is basically the website you're using for version control
+  # Right now, github is the only supported site
+  # If you're using an unsupported version control system, no worries,
+  # rusty-ci just wont run on pull requests.
+  version-control-system:
+    - github
+  # The username of the owner of the repository
+  owner:
+    - adam-mcdaniel
+
+  # The name of the repository
+  repo-name:
+    - rusty-ci
+
+  # You dont want to run arbitrary code on your machine when anyone
+  # makes a pull request. Rusty-CI will not test anyone's pull request
+  # if their username is not in this list.
+  whitelist:
+    - adam-mcdaniel
+
+  # This is the authentication token from your version control system
+  auth-token:
+    - "just-testing"
 
 
-/// This method takes a boxed buildsystem and runs its install routine
+# This section holds each worker
+# You can have as many workers as youd like, just be sure to fill out
+# each of their fields out properly.
+workers:
+  # The name of this worker is `test-worker`
+  test-worker:
+    # The ip of the master
+    masterhost:
+      - localhost
+    # The port of the master
+    # This is not the same as the web gui port!
+    masterport:
+      - 9989
+    # The absolute path to the working directory of this worker
+    basedir:
+      - '/home/adam/Desktop/rusty-ci/testing/test-worker'
+    # The password for this worker
+    # This is used by the master to give the worker a job
+    password:
+      - pass
+
+# This section holds each scheduler.
+# Like the workers section, you may have as many schedulers as youd like.
+schedulers:
+  # Create a scheduler named `ci-change`
+  # This scheduler will trigger the `rusty-ci-test` builder whenever it
+  # detects a change in a yaml file for any branch.
+  ci-change:
+    # This is a regular expression that matches a branch.
+    # If there is a change in a branch whos name matches this regex,
+    # it will be checked by the following triggers section.
+    # THIS WILL ONLY USE THE FIRST REGULAR EXPRESSION IN THIS SECTION TO MATCH THE BRANCH
+    branch:
+      - ".*"
+    # If a change has occurred in a branch that matches the regex in the branch section,
+    # Then the files that were changed are matched against the regular expressions in the
+    # triggers section. You can have any number of regular expressions in the triggers section.
+    # If any one of them matches the name of a file that was changed in a matched branch,
+    # then the builders in this scheduler's `builders` section are executed.
+    triggers:
+      - '.*\.yaml'
+      - '.*\.sh'
+      - '.*Makefile'
+    # This scheduler triggers the `rusty-ci-test` builder.
+    # You can put as many builders as youd like here, and the scheduler will start them all.
+    builders:
+      - rusty-ci-test
+
+# These are the builders that are executed by the schedulers
+# Each has its own specific task that is delegated to one or more workers
+# When a builder is run, its script is run on the command line.
+# You can have as many builders as youd like as well.
+builders:
+  # The name of the builder is `rusty-ci-test`
+  rusty-ci-test:
+    # This is the shell script that the workers will run when this builder is executed
+    # You can have as many instructions as youd like
+    # Mind you, you cannot use the |, >, <, >>, <<... operators. Sadly, buildbot
+    # passes each item separated by whitespace as another parameter to function.
+    script:
+      - echo Hello world!
+      - echo Im an instruction in a script!
+    # These are the workers to delegate this build job to
+    workers:
+      - test-worker
+    # The repo to refresh from before running
+    repo:
+      - "https://github.com/adam-mcdaniel/rusty-ci"
+"#)?;
+    info!("All done!");
+    Ok(())
+}
+
+
+
+/// This method takes a boxed BuildSystem trait object and runs its install routine
 fn install(mut b: Box<dyn BuildSystem>) {
     match b.install() {
         Ok(_) => {
--- src/helper/yaml.rs
+++ src/helper/yaml.rs
@@ -4,12 +4,26 @@ use rusty_yaml::Yaml;
 /// takes its first value, and converts it into a string,
 /// and trims quotation marks.
 pub fn unwrap<S: ToString>(yaml: &Yaml, section: S) -> String {
-    yaml.get_section(section.to_string())
+    let result = yaml.get_section(section.to_string())
         .unwrap()
         .nth(0)
         .unwrap()
-        .to_string()
-        .trim_matches('"')
-        .trim_matches('\'')
-        .to_string()
+        .to_string();
+
+	let first = match result.chars().nth(0) {Some(v) => v, None => ' '};
+	let last = match result.chars().nth(result.len()-1) {Some(v) => v, None => ' '};
+	if first == last {
+		match first {
+			// If the first and last character are the same, and are
+			// both forms of quotes, trim the outer most ones.
+			'\'' | '"' => {
+				result[1..result.len()-1].to_string()
+			},
+			_ => {
+				result.to_string()
+			}
+		}
+	} else {
+		result.to_string()
+	}
 }
