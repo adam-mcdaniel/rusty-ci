e7d9abc0090a90caead40288e415abc178e9c18c
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2019-06-24 10:21:35 -0400

    Save changes

--- src/buildbot/scheduler.rs
+++ src/buildbot/scheduler.rs
@@ -1,12 +1,24 @@
-use rusty_yaml::Yaml;
-use crate::unwrap;
-use std::process::exit;
-use std::fmt::{Display, Error, Formatter};
 
+use crate::unwrap;
+use rusty_yaml::Yaml;
+use std::fmt::{Display, Error, Formatter};
+use std::process::exit;
 
 pub struct Scheduler {
     /// Name of scheduler
     name: String,
+
+    /// This field is the password for whitelisting other people's pull requests. When
+    /// a non-whitelisted user makes a pull request, we don't want to test their code until
+    /// we know that it is safe. When an admin deems it's safe, they can use this password
+    /// to mark the pull/merge request as safe. After posting this password, the request
+    /// will permanently be marked as safe.
+    /// This password is a regular expression, so you can match multiple phrases or cases
+    /// if you'd like.
+    /// The reason it is in the scheduler is because we want to give permission to unknown users
+    /// on a per schedule basis. We want to have power over when a scheduler is allowed to assign
+    /// a job, not what job is assigned.
+    password: String,
 
     /// A regex expr that accepts a branch name.
     /// This scheduler will only operate on the
@@ -25,15 +37,34 @@ pub struct Scheduler {
 
 impl Scheduler {
     /// Create new scheduler
-    fn new<S>(name: S, branch: S, file_triggers: Vec<S>, buildernames: Vec<S>) -> Self
+    fn new<S>(name: S, password: S, branch: S, file_triggers: Vec<S>, buildernames: Vec<S>) -> Self
     where
         S: Display,
     {
         Self {
             name: name.to_string(),
+            password: password.to_string(),
             branch: branch.to_string(),
-            file_triggers: file_triggers.iter().map(|s| s.to_string().trim().trim_start_matches("\"").trim_end_matches("\"").to_string()).collect(),
-            buildernames: buildernames.iter().map(|s| s.to_string().trim().trim_start_matches("\"").trim_end_matches("\"").to_string()).collect(),
+            file_triggers: file_triggers
+                .iter()
+                .map(|s| {
+                    s.to_string()
+                        .trim()
+                        .trim_start_matches("\"")
+                        .trim_end_matches("\"")
+                        .to_string()
+                })
+                .collect(),
+            buildernames: buildernames
+                .iter()
+                .map(|s| {
+                    s.to_string()
+                        .trim()
+                        .trim_start_matches("\"")
+                        .trim_end_matches("\"")
+                        .to_string()
+                })
+                .collect(),
         }
     }
 }
@@ -50,6 +81,10 @@ def {name}_triggers(props):
 
     triggers = {triggers}
 
+    if not is_whitelisted(props, \"{password}\"):
+        print('NOT WHITELISTED!!!')
+        return []
+
     for f in props.files:
         for regex in triggers:
             if re.match(regex, str(f)):
@@ -65,6 +100,7 @@ c['schedulers'].append(schedulers.ForceScheduler(name=\"force_{name}\",
     builderNames={buildernames}))
 ",
             name = self.name.replace("-", "_"),
+            password = self.password.trim_matches('"'),
             branch = self.branch.trim_start_matches("\"").trim_end_matches("\""),
             triggers = format!("{:?}", self.file_triggers)
                 .replace("\\\"", "")
@@ -79,7 +115,7 @@ impl From<Yaml> for Scheduler {
     fn from(yaml: Yaml) -> Self {
         let name = yaml.get_name();
 
-        for section in ["branch", "triggers", "builders"].iter() {
+        for section in ["branch", "password", "triggers", "builders"].iter() {
             if !yaml.has_section(section) {
                 error!("There was an error creating a scheduler: The '{}' section is not specified for '{}'", section, name);
                 exit(1);
@@ -88,6 +124,7 @@ impl From<Yaml> for Scheduler {
 
 
         let branch: String = unwrap(&yaml, "branch");
+        let password: String = unwrap(&yaml, "password");
 
         let mut triggers = vec![];
         for trigger in yaml.get_section("triggers").unwrap() {
@@ -100,6 +137,6 @@ impl From<Yaml> for Scheduler {
             builders.push(builder.to_string());
         }
 
-        Scheduler::new(name, branch, triggers, builders)
+        Scheduler::new(name, password, branch, triggers, builders)
     }
 }
