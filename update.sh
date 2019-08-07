a2f54538c6c65701925d73f81f9880b9c803bfb0
adam-mcdaniel <adam.mcdanie17@gmail.com>
2019-08-07 12:49:58 -0400

    Source bashrc

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
+#! /bin/bash
 # To be run in linux container running rusty-ci!
+source ~/.bashrc
+
 cd ~
 git clone https://github.com/adam-mcdaniel/rusty-ci
 cd rusty-ci
 git pull origin master
+
 
 cargo install -f --path .
 . venv/bin/activate
