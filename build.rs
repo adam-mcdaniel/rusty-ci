15e5e754d6966194402a1df236c95b2e752de069
adam-mcdaniel <adam.mcdanie17@gmail.com>
2019-08-16 10:36:32 -0400

    Added comments to build.rs

--- build.rs
+++ build.rs
@@ -2,14 +2,19 @@ extern crate os_info;
 use os_info::{get, Type};
 
 
+/// Operating systems that Rusty-CI supports
 const SUPPORTED: &[Type] = &[
-    Type::Debian,
+    // We definitely support Ubuntu!
     Type::Ubuntu,
+    // We probably support Debian!
+    Type::Debian,
+    // We support linux?
     Type::Linux,
 ];
 
-
+/// Operating systems that Rusty-CI is incompatible with
 const INCOMPATIBLE: &[Type] = &[
+    // Windows is lame
     Type::Windows,
 ];
 
