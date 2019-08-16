31eaf1dfa32217210ed5c1f93563d32b8c90f8f3
adam-mcdaniel <adam.mcdanie17@gmail.com>
2019-08-16 10:21:34 -0400

    Added build.rs to detect OS and fail build on windows

---
+++ build.rs
@@ -0,0 +1,26 @@
+extern crate os_info;
+use os_info::{get, Type};
+
+
+const SUPPORTED: &[Type] = &[
+    Type::Debian,
+    Type::Ubuntu
+];
+
+
+const INCOMPATIBLE: &[Type] = &[
+    Type::Windows
+];
+
+
+fn main() {
+    let os_type = get().os_type();
+
+    if SUPPORTED.contains(&os_type) {
+        println!("Rusty-CI supports {}!", os_type);
+    } else if INCOMPATIBLE.contains(&os_type) {
+        panic!("{} is incompatible with Rusty-CI", os_type);
+    } else {
+        println!("Rusty-CI may not function properly on {}", os_type);    
+    }
+}
