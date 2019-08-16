993b323c7380496c4bc3eb9ac727e95e048a1098
adam-mcdaniel <adam.mcdanie17@gmail.com>
2019-08-16 13:25:53 -0400

    Cargo fmt

--- tests/string.rs
+++ tests/string.rs
@@ -1,48 +1,19 @@
 extern crate rusty_ci;
-use rusty_ci::{unquote};
-
+use rusty_ci::unquote;
 
 #[test]
 fn unquote_string() {
-    assert_eq!(
-        unquote("\"\"hey jude\""),
-        String::from("\"hey jude")
-    );
+    assert_eq!(unquote("\"\"hey jude\""), String::from("\"hey jude"));
 
-    assert_eq!(
-        unquote("\"\"hey jude\"\""),
-        String::from("\"hey jude\"")
-    );
+    assert_eq!(unquote("\"\"hey jude\"\""), String::from("\"hey jude\""));
 
-    assert_eq!(
-        unquote("'hey jude'"),
-        String::from("hey jude")
-    );
-    
-    assert_eq!(
-        unquote("''hey jude'"),
-        String::from("'hey jude")
-    );
+    assert_eq!(unquote("'hey jude'"), String::from("hey jude"));
 
-    assert_eq!(
-        unquote("'hey jude"),
-        String::from("'hey jude")
-    );
-    assert_eq!(
-        unquote("\"hey jude"),
-        String::from("\"hey jude")
-    );
-    assert_eq!(
-        unquote("hey jude'"),
-        String::from("hey jude'")
-    );
-    assert_eq!(
-        unquote("hey jude\""),
-        String::from("hey jude\"")
-    );
-    assert_eq!(
-        unquote("''hey jude''"),
-        String::from("'hey jude'")
-    );
+    assert_eq!(unquote("''hey jude'"), String::from("'hey jude"));
 
+    assert_eq!(unquote("'hey jude"), String::from("'hey jude"));
+    assert_eq!(unquote("\"hey jude"), String::from("\"hey jude"));
+    assert_eq!(unquote("hey jude'"), String::from("hey jude'"));
+    assert_eq!(unquote("hey jude\""), String::from("hey jude\""));
+    assert_eq!(unquote("''hey jude''"), String::from("'hey jude'"));
 }
